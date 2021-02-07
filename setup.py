c48326e1a6b1cd984f8f18cee7283dfad4913980
ggutierrez-bio <ggutierrez.bio@gmail.com>
2021-02-07 13:37:11 +0100

    setup ci

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup
 
 setup(
-    name='LigBinder',
-    version='0.1',
-    description='automatic targeted molecular dynamics for ligand bindning',
-    author='Guillermo Gutierrez',
-    author_email='',
-    url='https://github.com/ggutierrez-bio/ligbinder',
-    packages=['ligbinder'],
+    name="LigBinder",
+    version="0.1",
+    description="automatic targeted molecular dynamics for ligand bindning",
+    author="Guillermo Gutierrez",
+    author_email="",
+    url="https://github.com/ggutierrez-bio/ligbinder",
+    packages=["ligbinder"],
     inlcude_package_data=True,
     data_files=[("ligbinder", ["ligbinder/data/default_config.yml"])],
-    scripts=['bin/ligbinder'],
+    scripts=["bin/ligbinder"],
     # install_requires=["pytraj>=2"]
 )
