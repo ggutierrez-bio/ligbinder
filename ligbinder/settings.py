349e35a8eeca99ade3279a1da87c21f0ec3c80b7
ggutierrez-bio <ggutierrez.bio@gmail.com>
2021-02-07 14:35:22 +0100

    fix linting, running after tests for now

--- ligbinder/settings.py
+++ ligbinder/settings.py
@@ -46,7 +46,7 @@ class Settings:
         ]
         configs = [os.path.join(path, "default_config.yml") for path in home_candidates]
         logger.error(f"found configs: {configs}")
-        config = next(iter([config for config in configs if os.path.exists(config)]),"config.yml")
+        config = next(iter([config for config in configs if os.path.exists(config)]), "config.yml")
         logger.info(f"Using {config} for default settings")
         return config
 
